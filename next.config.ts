5af6fd1423d32c1ab8b3aa6c0168f2a7c93697ea
Manish kumar <107772484+hellotechmanish@users.noreply.github.com>
2025-08-10 11:00:30 +0530

    image render

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["images.unsplash.com"],
+    domains: [
+      "images.unsplash.com",
+      "raw.githubusercontent.com",
+      // add more domains if needed
+    ],
   },
+  // ...other config options
 };
 
 export default nextConfig;
